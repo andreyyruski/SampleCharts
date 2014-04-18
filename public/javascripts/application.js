44f0646dec564759ba0531071f347ea2262d7303
Navazish <navazish@webileapps.com>
2014-04-19 00:17:55 +0530

    Implemented proper legend names, bar-colors, values for Y-Axis

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,89 +7,98 @@ var attribute_names = [];
 var xScale = null
 var yScale = null;
 
-// Load the Source data
-$.get('/201594_data_source.xml',function(xml){
-    var data_rows = xml.getElementsByTagName("DATA");
-    //console.log(data_rows);
+// Load the Style data
+function load_style_data() {
+    $.get('/201594_style.xml',function(xml){
 
-    var first_row_attributes = data_rows[0].attributes;
-    var groups_length = first_row_attributes.length - 1;
+    });
+}
 
-    // Collect the Data-Attribute Names (Collecting the attributes of the first row)
-    for(var index=0; index < first_row_attributes.length; index++) {
-        attribute_names.push(first_row_attributes[index].localName)
-    }
+// Load the Source data and display the Graph
+function load_series_and_draw_graph() {
+    $.get('/201594_data_source.xml',function(xml){
+        var data_rows = xml.getElementsByTagName("DATA");
+        //console.log(data_rows);
 
-    // Populate dataset from the XML
-    // Loop for each attribute
-    for(var attribute_index=1; attribute_index<attribute_names.length; attribute_index++) {
-        attribute_data = [];
-        for(var data_row_index=0; data_row_index< data_rows.length; data_row_index++) {
-            attribute_data.push({x: data_row_index,
-                                 y: parseInt(data_rows[data_row_index].getAttribute(attribute_names[attribute_index]))})
+        var first_row_attributes = data_rows[0].attributes;
+        var groups_length = first_row_attributes.length - 1;
+
+        // Collect the Data-Attribute Names (Collecting the attributes of the first row)
+        for(var index=0; index < first_row_attributes.length; index++) {
+            attribute_names.push(first_row_attributes[index].localName)
         }
-        dataset.push(attribute_data);
-    }
 
-    // Populate the Group names
-    for(var data_row_index=0; data_row_index< data_rows.length; data_row_index++) {
-        group_names.push(data_rows[data_row_index].getAttribute(attribute_names[0]));
-    }
+        // Populate dataset from the XML
+        // Loop for each attribute
+        for(var attribute_index=1; attribute_index<attribute_names.length; attribute_index++) {
+            attribute_data = [];
+            for(var data_row_index=0; data_row_index< data_rows.length; data_row_index++) {
+                attribute_data.push({x: data_row_index,
+                    y: parseInt(data_rows[data_row_index].getAttribute(attribute_names[attribute_index]))})
+            }
+            dataset.push(attribute_data);
+        }
 
-    //Set up stack method
-    var stack = d3.layout.stack();
+        // Populate the Group names
+        for(var data_row_index=0; data_row_index< data_rows.length; data_row_index++) {
+            group_names.push(data_rows[data_row_index].getAttribute(attribute_names[0]));
+        }
 
-    //Data, stacked
-    stack(dataset);
+        //Set up stack method
+        var stack = d3.layout.stack();
 
-    //Set up scales
-     xScale = d3.scale.ordinal()
-        .domain(d3.range(dataset[0].length))
-        .rangeRoundBands([0, w], 0.05);
+        //Data, stacked
+        stack(dataset);
 
-     yScale = d3.scale.linear()
-        .domain([0,
-            d3.max(dataset, function(d) {
-                return d3.max(d, function(d) {
-                    return d.y0 + d.y;
-                });
+        //Set up scales
+        xScale = d3.scale.ordinal()
+            .domain(d3.range(dataset[0].length))
+            .rangeRoundBands([0, w], 0.05);
+
+        yScale = d3.scale.linear()
+            .domain([0,
+                d3.max(dataset, function(d) {
+                    return d3.max(d, function(d) {
+                        return d.y0 + d.y;
+                    });
+                })
+            ])
+            .range([h, 0]);
+
+        //Easy colors accessible via a 10-step ordinal scale
+        var colors = d3.scale.category10();
+
+        //Create SVG element
+        var svg = d3.select("body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+        // Add a group for each row of data
+        var groups = svg.selectAll("g")
+            .data(dataset)
+            .enter()
+            .append("svg:g")
+            .style("fill", function(d, i) {
+                return colors(i);
+            });
+
+        // Add a rect for each data value
+        var rects = groups.selectAll("rect")
+            .data(Object)
+            .enter()
+            .append("svg:rect")
+            .attr("x", function(d) {
+                return xScale(d.x);
             })
-        ])
-        .range([h, 0]);
-
-    //Easy colors accessible via a 10-step ordinal scale
-    var colors = d3.scale.category10();
-
-    //Create SVG element
-    var svg = d3.select("body")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h);
-
-    // Add a group for each row of data
-    var groups = svg.selectAll("g")
-        .data(dataset)
-        .enter()
-        .append("svg:g")
-        .style("fill", function(d, i) {
-            return colors(i);
-        });
-
-    // Add a rect for each data value
-    var rects = groups.selectAll("rect")
-        .data(Object)
-        .enter()
-        .append("svg:rect")
-        .attr("x", function(d) {
-            return xScale(d.x);
-        })
-        .attr("y", function(d) {
-            return  yScale(d.y0) - yScale(d.y);
-        })
-        .attr("height", function(d) {
-            return yScale(d.y);
-        })
-        .attr("width", xScale.rangeBand());
+            .attr("y", function(d) {
+                return  yScale(d.y0) - yScale(d.y);
+            })
+            .attr("height", function(d) {
+                return yScale(d.y);
+            })
+            .attr("width", xScale.rangeBand());
 
 
-});
+    });
+}
--- public/javascripts/stacked_bar.js
+++ public/javascripts/stacked_bar.js
@@ -1,4 +1,5 @@
 var dataset = null;
+var svg = null;
 var w = 640,
     h = 380,
     p = [120, 50, 30, 40],
@@ -10,125 +11,156 @@ var w = 640,
 var dataset = [];
 var group_names = [];
 var attribute_names = [];
+var bar_colors = [];
+var attribute_names_for_legend = [];
+var legend_actual_names = {};
 
-var svg = d3.select("body").append("svg:svg")
-    .attr("width", w)
-    .attr("height", h)
-    .append("svg:g")
-    .attr("transform", "translate(" + p[3] + "," + (h - p[2]) + ")");
+function init_svg() {
+    svg = d3.select("body").append("svg:svg")
+        .attr("width", w)
+        .attr("height", h)
+        .append("svg:g")
+        .attr("transform", "translate(" + p[3] + "," + (h - p[2]) + ")");
+}
 
-d3.xml("/201594_data_source.xml", function(xml) {
-    var data_rows = xml.getElementsByTagName("DATA");
-    //console.log(data_rows);
+function load_data_and_draw_graph() {
+    d3.xml("/201594_data_source.xml", function(xml) {
+        var data_rows = xml.getElementsByTagName("DATA");
+        //console.log(data_rows);
 
-    var first_row_attributes = data_rows[0].attributes;
-    var groups_length = first_row_attributes.length - 1;
+        var first_row_attributes = data_rows[0].attributes;
+        var groups_length = first_row_attributes.length - 1;
 
-    // Collect the Data-Attribute Names (Collecting the attributes of the first row)
-    for(var index=0; index < first_row_attributes.length; index++) {
-        attribute_names.push(first_row_attributes[index].localName)
-    }
-
-    // Populate dataset from the XML
-    // Loop for each attribute
-    for(var attribute_index=1; attribute_index<attribute_names.length; attribute_index++) {
-        attribute_data = [];
-        for(var data_row_index=0; data_row_index< data_rows.length; data_row_index++) {
-            attribute_data.push({x: data_row_index,
-                y: parseInt(data_rows[data_row_index].getAttribute(attribute_names[attribute_index]))})
+        // Collect the Data-Attribute Names (Collecting the attributes of the first row)
+        for(var index=0; index < first_row_attributes.length; index++) {
+            attribute_names.push(first_row_attributes[index].localName)
         }
-        dataset.push(attribute_data);
-    }
 
-    // Populate the Group names
-    for(var data_row_index=0; data_row_index< data_rows.length; data_row_index++) {
-        group_names.push(data_rows[data_row_index].getAttribute(attribute_names[0]));
-    }
+        // Populate dataset from the XML
+        // Loop for each attribute
+        for(var attribute_index=1; attribute_index<attribute_names.length; attribute_index++) {
+            attribute_data = [];
+            for(var data_row_index=0; data_row_index< data_rows.length; data_row_index++) {
+                attribute_data.push({x: data_row_index,
+                    y: parseInt(data_rows[data_row_index].getAttribute(attribute_names[attribute_index]))})
+            }
+            dataset.push(attribute_data);
+        }
 
-    //Set up stack method
-    var stack = d3.layout.stack();
+        // Populate the Group names
+        for(var data_row_index=0; data_row_index< data_rows.length; data_row_index++) {
+            group_names.push(data_rows[data_row_index].getAttribute(attribute_names[0]));
+        }
 
-    //Data, stacked
-    stack(dataset);
+        //Set up stack method
+        var stack = d3.layout.stack();
 
-    var colors = d3.scale.category10();
+        //Data, stacked
+        stack(dataset);
 
-    // Compute the x-domain (by date) and y-domain (by top).
-    x.domain(dataset[0].map(function(d) { return d.x; }));
-    y.domain([0, d3.max(dataset[dataset.length - 1], function(d) { return d.y0 + d.y; })]);
+        var colors = d3.scale.category10();
 
-    // Add a group for each cause.
-    var cause = svg.selectAll("g.cause")
-        .data(dataset)
-        .enter().append("svg:g")
-        .attr("class", "cause")
-        .style("stroke", "black")
-        .style("fill", function(d, i) {
-            return colors(i);
+        // Compute the x-domain (by date) and y-domain (by top).
+        x.domain(dataset[0].map(function(d) { return d.x; }));
+        y.domain([0, d3.max(dataset[dataset.length - 1], function(d) { return d.y0 + d.y; })]);
+
+        // Add a group for each cause.
+        var cause = svg.selectAll("g.cause")
+            .data(dataset)
+            .enter().append("svg:g")
+            .attr("class", "cause")
+            .style("stroke", "black")
+            .style("fill", function(d, i) {
+                return bar_colors[i];
+            });
+
+        // Add a rect for each date.
+        var rect = cause.selectAll("rect")
+            .data(Object)
+            .enter().append("svg:rect")
+            .attr("x", function(d) { return x(d.x); })
+            .attr("y", function(d) { return -y(d.y0) - y(d.y); })
+            .attr("height", function(d) { return y(d.y); })
+            .attr("width", x.rangeBand());
+
+        // Add a label per date.
+        var label = svg.selectAll("text")
+            .data(x.domain())
+            .enter().append("svg:text")
+            .attr("x", function(d) { return x(d) + x.rangeBand() / 2; })
+            .attr("y", 6)
+            .attr("text-anchor", "middle")
+            .attr("dy", ".71em")
+            .text(function(d) {return group_names[d];});
+
+        // Add y-axis rules.
+        var rule = svg.selectAll("g.rule")
+            .data(y.ticks(10))
+            .enter().append("svg:g")
+            .attr("class", "rule")
+            .attr("transform", function(d) { return "translate(0" + "," + -y(d) + ")"; });
+
+        rule.append("svg:line")
+            .attr("x2", w - p[1] - p[3])
+            .style("stroke", function(d) { return d ? "#fff" : "#000"; })
+            .style("stroke-opacity", function(d) { return d ? 0 : null; });
+
+        rule.append("svg:text")
+            .attr("x", w - p[1] - p[3] + 6)
+            .attr("dy", ".35em")
+            .text(d3.format(",d"));
+
+
+
+        attribute_names_for_legend = attribute_names.splice(1,attribute_names.length)
+        legend_scale = d3.scale.linear().domain([0,attribute_names_for_legend.length]).range([0, w - p[1] - p[3]])
+        var legend = svg.append("g");
+        legend.selectAll("rect")
+            .data(attribute_names_for_legend)
+            .enter()
+            .append("svg:rect")
+            .attr("x",function(d,i) {return legend_scale(i)})
+            .attr("y",10)
+            .attr("width",20)
+            .attr("height", 20)
+            .text(function(d) {return "Amma";})
+            .style("fill",function(d,i){console.log(bar_colors[i]); return bar_colors[i];})
+            .attr("transform", "translate(0,-"+(h - p[0] - p[2]+120)+")");
+
+        legend.selectAll("text")
+            .data(attribute_names_for_legend)
+            .enter()
+            .append("svg:text")
+            .attr("x",function(d,i) {return legend_scale(i) + 25})
+            .attr("y",25)
+            .attr("width",20)
+            .attr("height", 20)
+            .text(function(d,i) {return legend_actual_names[attribute_names_for_legend[i]];})
+            .style("fill","black")
+            .attr("transform", "translate(0,-"+(h - p[0] - p[2]+120)+")");
+    });
+}
+
+function load_style_data() {
+    $.get("/201594_style.xml", function(xml_data) {
+        var xml = $(xml_data);
+        var series_strings = ["firstSeries", "secondSeries", "thirdSeries", "fourthSeries", "fifthSeries", "sixthSeries"]
+        for(var index = 0; index < series_strings.length; index++) {
+            bar_colors.push(parseInt(xml.find("style[d='."+series_strings[index]+"'][n='fill']").attr("v")).toString(16));
+        }
+        load_series_data();
+    });
+}
+
+function load_series_data() {
+    d3.csv('/201594_series.csv',function(csv){
+        csv.map(function(row){
+            legend_actual_names[row['DATA_SERIES_NAME']] = row['DISPLAY_NAME'];
         });
+        load_data_and_draw_graph();
+    });
+}
 
-    // Add a rect for each date.
-    var rect = cause.selectAll("rect")
-        .data(Object)
-        .enter().append("svg:rect")
-        .attr("x", function(d) { return x(d.x); })
-        .attr("y", function(d) { return -y(d.y0) - y(d.y); })
-        .attr("height", function(d) { return y(d.y); })
-        .attr("width", x.rangeBand());
+init_svg();
+load_style_data();
 
-    // Add a label per date.
-    var label = svg.selectAll("text")
-        .data(x.domain())
-        .enter().append("svg:text")
-        .attr("x", function(d) { return x(d) + x.rangeBand() / 2; })
-        .attr("y", 6)
-        .attr("text-anchor", "middle")
-        .attr("dy", ".71em")
-        .text(function(d) {return group_names[d];});
-
-    // Add y-axis rules.
-    var rule = svg.selectAll("g.rule")
-        .data(y.ticks(5))
-        .enter().append("svg:g")
-        .attr("class", "rule")
-        .attr("transform", function(d) { return "translate(0" + "," + -y(d) + ")"; });
-
-    rule.append("svg:line")
-        .attr("x2", w - p[1] - p[3])
-        .style("stroke", function(d) { return d ? "#fff" : "#000"; })
-        .style("stroke-opacity", function(d) { return d ? 0 : null; });
-
-    rule.append("svg:text")
-        .attr("x", w - p[1] - p[3] + 6)
-        .attr("dy", ".35em")
-        .text(d3.format(",d"));
-
-
-
-    attribute_names_for_legend = attribute_names.splice(1,attribute_names.length)
-    legend_scale = d3.scale.linear().domain([0,attribute_names_for_legend.length]).range([0, w - p[1] - p[3]])
-    var legend = svg.append("g");
-    legend.selectAll("rect")
-          .data(attribute_names_for_legend)
-          .enter()
-          .append("svg:rect")
-        .attr("x",function(d,i) {return legend_scale(i)})
-        .attr("y",10)
-        .attr("width",20)
-        .attr("height", 20)
-        .text(function(d) {return "Amma";})
-        .style("fill",function(d,i){console.log(colors(i)); return colors(i);})
-        .attr("transform", "translate(0,-"+(h - p[0] - p[2]+120)+")");
-
-    legend.selectAll("text")
-        .data(attribute_names_for_legend)
-        .enter()
-        .append("svg:text")
-        .attr("x",function(d,i) {return legend_scale(i) + 25})
-        .attr("y",25)
-        .attr("width",20)
-        .attr("height", 20)
-        .text(function(d,i) {return attribute_names_for_legend[i];})
-        .style("fill","black")
-        .attr("transform", "translate(0,-"+(h - p[0] - p[2]+120)+")");
-});
